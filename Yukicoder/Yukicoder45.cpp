a818f3f9497875ff1cd36034bee30dc5dfa7e55e
kani1128 <tmoiki1128@gmail.com>
2015-09-30 00:31:49 +0900

    Create Yukicoder45.cpp

---
+++ Yukicoder/Yukicoder45.cpp
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <set>
+#include <map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <ctype.h>
+#include <ctime>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <bitset>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <utility>
+#include <numeric>
+#include <complex>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <cassert>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#define aLL(g) (g).begin(),(g).end()
+#define REP(i, x, n) for(int i = x; i < n; i++)
+#define rep(i,n) REP(i,0,n)
+#define F(i,j,k) fill(i[0],i[0]+j*j,k)
+#define P(p) cout<<(p)<<endl;
+#define EXISt(s,e) ((s).find(e)!=(s).end())
+#define INF 1<<25
+#define MAX 100000000
+#define PI 3.14159265258979
+#define pb push_back
+using namespace std;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<double> vd;
+typedef pair<int, int> pii;
+typedef pair<long, long> pll;
+typedef long long ll;
+int dx[] = { 0,1,0,-1 };
+int dy[] = { -1,0,1,0 };
+
+int sttoi(std::string str) { 
+	int ret; 
+	std::stringstream ss; ss << str;
+	ss >> ret; 
+	return ret; 
+}
+
+bool sort_greater(const pair<string, int> &a, const pair<string, int> &b) {
+	return a.second > b.second;
+}
+
+
+void solve() {
+	int dp[1000];
+	int n,sushi[1000];
+	cin >> n;
+	rep(i, n) {
+		cin >> sushi[i];
+	}
+	dp[0] = sushi[0];
+	dp[1] = max(sushi[0],sushi[1]);
+	REP(i, 2, n) {
+		dp[i] = max(dp[i - 1], dp[i - 2] + sushi[i]);
+	}
+	P(dp[n - 1]);
+}
+
+int main() {
+	solve();
+	return 0;
+}
